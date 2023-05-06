270c84843a535110daddc5af13f2405bf0234eef
akbansode4 <akbansode4@gmail.com>
2023-05-06 14:42:37 +0530

    variable assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+var Name = 'Akshay Bansode';
+var fullName = 'Akshay Shantilal Bansode';
+var firstName, lastName, age, city, pincode, gender;
+firstName = 'Akshay';
+lastName = 'Bansode';
+age = 29;
+city = 'Sangola, Solapur'
+pincode = 413307
+gender = 'Male'
+
+console.log(Name);
+console.log(fullName);
+console.log('I Love Only JS');
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(city);
+console.log(pincode);
+console.log(gender);
+console.log('No');
---
+++ hello.js
@@ -0,0 +1,6 @@
+var x;   /* Veriable declaration without Intialization value is undefined */
+var age = Number;   //Veriable declaration It comes in Green color
+var firstName = 'Akshay';
+age = 22   // Veriable Intialization
+age = 29  // Veriable Update
+console.log(firstName);
